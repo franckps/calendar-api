ef8e85b6418cc68e57205237750e71b53ca3b09d
Francisco Pereira <fran.p.s@hotmail.com>
2021-12-23 23:31:35 -0300

    feat: add create calendar feature

--- src/calendar/calendar.controller.ts
+++ src/calendar/calendar.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateCalendarProtocol } from 'src/protocols/calendar-protocols';
 import { CalendarService } from './calendar.service';
-@Controller()
+@Controller('events')
 export class CalendarController {
   constructor(private readonly calendarService: CalendarService) {}
 
   @Get()
   async findAll(): Promise<any> {
     return this.calendarService.findAll();
+  }
+  
+  @Post()
+  async create(@Body() body: CreateCalendarProtocol): Promise<any> {
+    console.log('controller: ',{body})
+    return this.calendarService.create(body);
   }
 }
--- src/calendar/calendar.service.ts
+++ src/calendar/calendar.service.ts
@@ -2,12 +2,33 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Calendar } from '../entity/calendar.entity';
 import { Repository } from 'typeorm';
+import { CalendarProtocol, CreateCalendarProtocol } from 'src/protocols/calendar-protocols';
+import { randomUUID } from 'crypto'
 
 @Injectable()
 export class CalendarService {
     constructor(@InjectRepository(Calendar) private calendarRepository: Repository<Calendar>){}
 
-    async findAll() {
+    async create({ title, description, date, start_time, end_time } : CreateCalendarProtocol): Promise<CalendarProtocol> {
+        console.log('service: ',{title, description, date, start_time, end_time})
+        const id = randomUUID();
+        const created_at = (new Date(Date.now())).toUTCString()
+        const active = true;
+        const calendar = new Calendar()
+        calendar.title = title;
+        calendar.description = description;
+        calendar.date = date;
+        calendar.start_time = start_time;
+        calendar.end_time = end_time;
+        calendar.id = id;
+        calendar.created_at = created_at;
+        calendar.active = active;
+
+        await this.calendarRepository.save(calendar)
+        return { id, created_at, active, title, description, date, start_time, end_time }
+    }
+
+    async findAll(): Promise<CalendarProtocol[]> {
         return this.calendarRepository.find()
     }
 }
